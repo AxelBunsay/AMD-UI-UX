c61f0538aa5c7b29fff19b3ac5aa4930efa8ffb0
AxelBunsay <akurunogia12@gmail.com>
2025-10-03 15:38:10 +0800

    AMD UIX

---
+++ shop.js
@@ -0,0 +1,4 @@
+// shop.js
+document.getElementById('shopBtn').addEventListener('click', function() {
+    window.open('https://www.amd.com/en/shop', '_blank');
+});
---
+++ subscribe.js
@@ -0,0 +1,4 @@
+// subscribe.js
+document.getElementById('subscribeBtn').addEventListener('click', function() {
+    alert('Thank you for subscribing to AMD updates!');
+});
